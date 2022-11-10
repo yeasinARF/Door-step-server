218040d4183986c16d55b8c50ab9771e1c21bde6
Asif Farhan <asifhossain953@gmail.com>
2022-11-10 10:37:47 +0600

    server site

--- index.js
+++ index.js
@@ -13,13 +13,18 @@ app.use(express.json());
 // console.log(process.env.DB_PASSWORD)
 
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0ww6vlu.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run(){
+    try{
+        const serviceCollection=client.db('doorStep').collection('services')
+    }
+    finally{
+
+    }
+}
+
+run().catch(err=>console.error(err));
 
 app.get('/', (req, res) => {
     res.send('Door Step Server Running');
