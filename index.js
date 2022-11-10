f5de87b719e7f60f240020a4fb93ed75481e136b
Asif Farhan <asifhossain953@gmail.com>
2022-11-10 16:33:51 +0600

    add objectid

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 // middle Wars 
 app.use(cors());
 app.use(express.json());
@@ -12,19 +14,39 @@ app.use(express.json());
 // console.log(process.env.DB_USER);
 // console.log(process.env.DB_PASSWORD)
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0ww6vlu.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0ww6vlu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-        const serviceCollection= client.db('doorStep').collection('services')
+async function run() {
+    try {
+        const serviceCollection = client.db('doorStep').collection('services')
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
+        app.get('/allServices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        
+        app.get("/allServices/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            console.log(query);
+            res.send(service);
+        });
     }
-    finally{
+    finally {
 
     }
 }
 
-run().catch(err=>console.error(err));
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('Door Step Server Running');
