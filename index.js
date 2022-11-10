4e00a04e19de63c695b5435df24b1d2f49edd26c
Asif Farhan <asifhossain953@gmail.com>
2022-11-10 19:51:55 +0600

    add service sort

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -22,7 +22,7 @@ async function run() {
         const serviceCollection = client.db('doorStep').collection('services')
         app.get('/services', async (req, res) => {
             const query = {}
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).sort({currentTime:-1});
             const services = await cursor.limit(3).toArray();
             res.send(services);
         })
@@ -32,13 +32,19 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
-        
+
         app.get("/allServices/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             console.log(query);
             res.send(service);
+        });
+        app.post("/allServices", async (req, res) => {
+            const service = req.body;
+            console.log(service);
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
         });
     }
     finally {
